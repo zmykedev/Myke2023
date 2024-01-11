920938e419ee39d12353bbc3b6fde408503fca90
zmykedev <zmaikol399@gmail.com>
2024-01-11 12:15:43 -0300

    testimonial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ParallaxProvider } from "react-scroll-parallax";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ParallaxProvider>
+      <App />
+    </ParallaxProvider>
   </React.StrictMode>,
 );
